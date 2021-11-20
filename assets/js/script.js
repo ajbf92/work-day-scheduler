7b58521d9903273ea883da58b83759f0c772690c
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2021-11-20 15:31:27 -0500

    add set interval function for date/timer and allow text area to be edited

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
+// current date and time to change every second 
 const dateTime = new Date();
 document.getElementById("currentDay").innerHTML = dateTime;
 
+// function to make the date and time update every second
+setInterval(function(){
+    document.getElementById("currentDay").innerHTML = new Date()}, 1000)
 
+// adding/editing text area
+$(".description").on("click", "textarea", function () {
+    var textInput = $("<textarea>").addClass("form-control").val(text);
+    textInput.trigger("focus");
+  });
