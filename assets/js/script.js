9001159d6e18cd01b71832e55967dfb218085c17
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2021-11-20 15:40:19 -0500

    create save event function to send info to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,29 @@ $(".description").on("click", "textarea", function () {
     var textInput = $("<textarea>").addClass("form-control").val(text);
     textInput.trigger("focus");
   });
+
+// saving the data to local storage
+var saveEvent = function (event) {
+    // targeting the area to get the id
+    const textareaId = "x-" + event.target.id;
+
+    // extracting the value of the area selected
+    const eventDescription = document.getElementById(textareaId).value;
+
+    let descriptionExist = false;
+    // loop running through the events array 
+    for (i = 0; i < eventsArr.length; i++)
+    // checking if the id exist already if so update with new description
+    if (eventsArr[i].id === textareaId) {
+        eventsArr[i].description = eventDescription;
+        descriptionExist = true;
+        break;
+    }
+    // if the id does not exist in the array add the id with the description
+    if (!descriptionExist) {
+        eventsArr.push({ id: textareaId, description: eventDescription });
+    }
+    // send the information collected into local storage
+    localStorage.setItem("Event Data", JSON.stringify(eventsArr));
+  };
+  
